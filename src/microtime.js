fc5b0201d02ec50cc685f8b0cf645500cc40882c
Christopher Davenport <chris@christopherdavenport.tech>
2021-01-05 13:20:14 -0800

    Update microtime.js

--- src/microtime.js
+++ src/microtime.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const time = Date.now() * 1e3
-const start = process.hrtime()
+const time = Date.now() * 1e3 // Program start time in microseconds
+const start = process.hrtime() // Program start hrtime used to calculate running time
 
 module.exports.now = function () {
-  const diff = process.hrtime(start)
-  return time + diff[0] * 1e6 + Math.round(diff[1] * 1e-3)
+  const diff = process.hrtime(start) // Amount of time application has been running for betwwen start and now
+  return time + diff[0] * 1e6 + Math.round(diff[1] * 1e-3) // diff[0] is seconds, diff[1] is nanos
 }
