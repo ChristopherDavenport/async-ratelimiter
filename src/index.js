caebb0aa27196c62db931317f6f9f28aac178332
Christopher Davenport <chris@christopherdavenport.tech>
2021-01-05 13:53:23 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ const assert = require('assert')
 
 const microtime = require('./microtime')
 
+// String to number in base10
 const toNumber = str => parseInt(str, 10)
 
 module.exports = class Limiter {
+  // max: The maximum number of requests within duration.
+  // duration: How long keep records of requests in milliseconds.
+  // namespace: The prefix used for compound the key.
   constructor ({ id, db, max = 2500, duration = 3600000, namespace = 'limit' }) {
     assert(db, 'db required')
     this.db = db
@@ -26,25 +30,33 @@ module.exports = class Limiter {
     assert(max, 'max required')
     assert(duration, 'duration required')
 
-    const key = `${this.namespace}:${id}`
-    const now = microtime.now()
-    const start = now - duration * 1000
+    const key = `${this.namespace}:${id}` // compound key
+    const now = microtime.now() // current time in microseconds since 
+
+    // what position to drop all entries before current microseconds 
+    // converted to microseconds
+    const start = now - duration * 1000 - duration 
 
     const operations = [
-      ['zremrangebyscore', key, 0, start],
-      ['zcard', key],
-      ['zrange', key, 0, 0],
-      ['zrange', key, -max, -max],
-      ['pexpire', key, duration]
+      ['zremrangebyscore', key, 0, start], // removes all entries before start period, guarantees removal of expired keys
+      ['zcard', key], // count
+      // zadd spliced into this position
+      ['zrange', key, 0, 0], // oldest -  first element in the set
+      ['zrange', key, -max, -max], // oldestInRange - n elements that are within the set, 
+      // gets the element at last position allowed in size by max 
+      ['pexpire', key, duration] // Expire within redis even if unaccessed
     ]
 
     if (decrease) operations.splice(2, 0, ['zadd', key, now, now])
 
     const res = await this.db.multi(operations).exec()
+   
     const count = toNumber(res[1][1])
     const oldest = toNumber(res[decrease ? 3 : 2][1])
     const oldestInRange = toNumber(res[decrease ? 4 : 3][1])
-    const resetMicro =
+    const resetMicro = // ? If there are not more than max elements then return oldest
+      // otherwise return element at position oldest in range whix is the maximum size of entries
+      // Rate limiting period will end at duration length from that point
       (Number.isNaN(oldestInRange) ? oldest : oldestInRange) + duration * 1000
 
     return {
